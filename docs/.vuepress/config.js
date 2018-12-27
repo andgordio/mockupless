8063206e85afef7a09f9b612f6b64e20c9250527
And Gordy <andgordio@gmail.com>
2018-12-27 23:48:34 +0200

    Complex conditions

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -79,7 +79,7 @@ module.exports = {
         //   collapsable: false,
         //   children: [
         //     'Functions/functions',
-        //     ....
+        //     ...
         //   ]
         // },
         // {
